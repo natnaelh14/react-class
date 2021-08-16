cfd730b5f967ee612dde69e9cd0bb2334aac7d65
natnaelh14 <natnaelh14@github.com>
2021-08-16 10:49:33 -0400

    react-activity-08

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
-// TODO: Add a comment explaining what this import statement is doing
-import HelloReact from './components/HelloReact';
+import React from "react";
+import JSXVariables from "./components/JSXVariables";
 
-// TODO: Add a comment explaining the purpose of the App component
 function App() {
-  return <HelloReact />;
+  return <JSXVariables />;
 }
 
 export default App;
