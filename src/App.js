b3526cb98528f6d591c13762daa22a925e4b1eef
natnaelh14 <natnaelh14@github.com>
2021-08-17 10:20:57 -0400

    react-activity-21

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
-import OmdbContainer from "./components/OmdbContainer";
+import React from 'react';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Card from './components/Card';
 
+// In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {
-  return <OmdbContainer />;
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 }
 
 export default App;
